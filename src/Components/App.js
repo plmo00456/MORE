e87b241b5feee119a0ffc2c657446ab6ce2bdd89
ParkWJ <park_wj7269@naver.com>
2022-01-24 16:04:10 +0900

    헤더, 백그라운드 비디오

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,49 +1,102 @@
-import logo from './images/main-logo.png';
-import './css/Header.scss'
-import 'reset-css';
-import React, { useState, useEffect } from 'react';
-import customAxios from './customAxios';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link
-} from 'react-router-dom';
+import logo from "./images/logo.png";
+import "reset-css";
+import "./css/Header.scss";
+import "./css/Main.scss";
+import React, { useState, useEffect } from "react";
 
-import './App.css';
+import videoSrc from "./video/out.mp4";
+import customAxios from "./customAxios";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./App.css";
+
+import Movie from "./menu/movie/movie";
 
 function App() {
+  useEffect(() => {
+    const mainMenu = document.querySelectorAll(".main-menu>li");
+    mainMenu.forEach((menu) => {
+      menu.addEventListener("mouseenter", (e) => {
+        menu.classList.add("active");
+      });
+      menu.addEventListener("mouseleave", (e) => {
+        menu.classList.remove("active");
+      });
+    });
+  }, []);
+
   return (
     <Router>
-      <div className='Header'>
-        <Link to="/movie">
-          <img
-            className='logo'
-            src={logo}
-            alt='로고'
-          />
-        </Link>
-        
-        <nav>
-          <ul className='main-menu'>
-            <li>
-              <Link to="/">홈</Link>
-            </li>
-            <li>
-              <Link to="/movie">영화</Link>
-              <ul className='sub-menu'>
-                <li><Link to="/movie">소메뉴1</Link></li>
-                <li><Link to="/movie">소메뉴2</Link></li>
-                <li><Link to="/movie">소메뉴3</Link></li>
-              </ul>
-            </li>
-            <li>
-              <Link to="/users">사용자</Link>
-            </li>
-          </ul>
-        </nav>
-
-
+      <div className="Wrap">
+        <div className="Header">
+          <div className="Container">
+            <div className="logo-menu">
+              <nav>
+                <Link to="/">
+                  <img className="logo" src={logo} alt="로고" />
+                </Link>
+                <ul className="main-menu">
+                  <li>
+                    <Link to="/">홈</Link>
+                  </li>
+                  <li>
+                    <Link to="/movie">영화</Link>
+                    <ul className="sub-menu">
+                      <li>
+                        <Link to="/movie">소메뉴1</Link>
+                      </li>
+                      <li>
+                        <Link to="/movie">소메뉴2</Link>
+                      </li>
+                      <li>
+                        <Link to="/movie">소메뉴3</Link>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <Link to="/users">드라마</Link>
+                  </li>
+                </ul>
+              </nav>
+              <div className="right">
+                <ul>
+                  <li>
+                    <Link to="/">로그인</Link>
+                  </li>
+                  <li>
+                    <Link to="/">회원가입</Link>
+                  </li>
+                  <li>
+                    <FontAwesomeIcon
+                      className="search"
+                      icon={faSearch}
+                      size="1x"
+                    />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="Main">
+          <div className="Background-video">
+            <div>
+              <h1>MORE</h1>
+              <h2>All Reviews.</h2>
+            </div>
+            <video loop muted autoPlay>
+              <source src={videoSrc}></source>
+            </video>
+          </div>
+          <div className="Container">
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="movie" element={<Movie />} />
+              <Route path="users" element={<Users />} />
+            </Routes>
+          </div>
+        </div>
       </div>
     </Router>
   );
@@ -52,38 +105,18 @@ function App() {
 export default App;
 
 function Main() {
-  // IP주소 변수 선언
-  const [ip, setIp] = useState('');
-
-  // IP주소 값을 설정합니다.
-  function callback(data) {
-      setIp(data);
-  }
-
-  // 첫번째 렌더링을 다 마친 후 실행합니다.
-  useEffect(
-      () => {
-          // 클라이언트의 IP주소를 알아내는 백엔드의 함수를 호출합니다.
-          customAxios('/ip', callback);
-      }, []
-  );
-
-  return (
-      <header className="App-header">
-          이 기기의 IP주소는 {ip}입니다.
-      </header>
-  );
+  return <div></div>;
 }
 
 function Users() {
   return (
-      <div>
-          <hr />
-          <h2>사용자 페이지</h2>
-          <div className='parent'>
-              <div className='child'></div>
-          </div>
-          <div className='friend'></div>
+    <div>
+      <hr />
+      <h2>사용자 페이지</h2>
+      <div className="parent">
+        <div className="child"></div>
       </div>
+      <div className="friend"></div>
+    </div>
   );
 }
