edee6bf2cc23a812bc719d351117ebe0f09e6d92
ParkWJ <park_wj7269@naver.com>
2022-01-25 14:05:12 +0900

    메인 영화 포스터

--- src/Components/App.js
+++ src/Components/App.js
@@ -16,6 +16,12 @@ import Movie from "./menu/movie/movie";
 
 function App() {
   useEffect(() => {
+    const meta = document.createElement("meta");
+    meta.name = "viewport";
+    meta.content =
+      "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover";
+    document.getElementsByTagName("head")[0].appendChild(meta);
+
     const mainMenu = document.querySelectorAll(".main-menu>li");
     mainMenu.forEach((menu) => {
       menu.addEventListener("mouseenter", (e) => {
--- src/Components/menu/main/main.js
+++ src/Components/menu/main/main.js
@@ -5,24 +5,49 @@ export default function Main() {
     <div className="block-wrap">
       <div className="block movie">
         <div className="title">
-          <span>영화</span>
+          <span>Movie</span>
           <div className="tabs">
             <input type="radio" id="radio-1" name="tabs" defaultChecked />
             <label className="tab" htmlFor="radio-1">
-              Upcoming
+              Tab1
             </label>
             <input type="radio" id="radio-2" name="tabs" />
             <label className="tab" htmlFor="radio-2">
-              Development
+              Tab2
             </label>
             <input type="radio" id="radio-3" name="tabs" />
             <label className="tab" htmlFor="radio-3">
-              Completed
+              Tab3
             </label>
             <span className="glider"></span>
           </div>
         </div>
-        <div className="list"></div>
+        <div className="items">
+          <div className="item">
+            <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/6zt5l4DYV1kLPL5Vqz1kLq3THXD.jpg" />
+          </div>
+          <div className="item">
+            <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/6zt5l4DYV1kLPL5Vqz1kLq3THXD.jpg" />
+          </div>
+          <div className="item">
+            <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/6zt5l4DYV1kLPL5Vqz1kLq3THXD.jpg" />
+          </div>
+          <div className="item">
+            <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/6zt5l4DYV1kLPL5Vqz1kLq3THXD.jpg" />
+          </div>
+          <div className="item">
+            <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/6zt5l4DYV1kLPL5Vqz1kLq3THXD.jpg" />
+          </div>
+          <div className="item">
+            <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/6zt5l4DYV1kLPL5Vqz1kLq3THXD.jpg" />
+          </div>
+          <div className="item">
+            <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/6zt5l4DYV1kLPL5Vqz1kLq3THXD.jpg" />
+          </div>
+          <div className="item">
+            <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/6zt5l4DYV1kLPL5Vqz1kLq3THXD.jpg" />
+          </div>
+        </div>
       </div>
       <div className="block drama"></div>
     </div>
